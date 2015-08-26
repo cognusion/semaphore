bfe2896f23952dc57ae376fc730e2eebfc4429bb
Matt Keller <m@cognusion.com>
2015-08-26 15:50:22 -0400

    Last godoc tweak? Please?!

--- semaphore.go
+++ semaphore.go
@@ -1,44 +1,44 @@
 /*
-package main
-
-import (
-	"github.com/cognusion/semaphore"
-	"time"
-	"fmt"
-)
-
-func main() {
-	// Make a new semaphore, with the number of
-	// simultaneous locks you want to allow
-	S := NewSemaphore(1)
+	package main
 	
-	go func() {
-		// Call lock, which will block if there aren't free locks
-		// and defer the unlock until the function ends
-		S.Lock()
-		defer S.Unlock()
+	import (
+		"github.com/cognusion/semaphore"
+		"time"
+		"fmt"
+	)
 	
-		// Do some stuff
-		fmt.Println("Doing some stuff")
-		time.Sleep(1 * time.Second)
-	}()
-	
-	go func() {
-		// Call lock, which will block if there aren't free locks
-		// and defer the unlock until the function ends
-		S.Lock()
-		defer S.Unlock()
-	
-		// Do some other stuff
-		fmt.Println("Doing some other stuff")
-		time.Sleep(50 * time.Millisecond)
-	}()
-	
-	time.Sleep(1 * time.Millisecond)
-	fmt.Printf("Free locks? %d\n",S.Free())
-	time.Sleep(3 * time.Second)
-	fmt.Printf("Free locks now? %d\n",S.Free())
-}
+	func main() {
+		// Make a new semaphore, with the number of
+		// simultaneous locks you want to allow
+		S := NewSemaphore(1)
+		
+		go func() {
+			// Call lock, which will block if there aren't free locks
+			// and defer the unlock until the function ends
+			S.Lock()
+			defer S.Unlock()
+		
+			// Do some stuff
+			fmt.Println("Doing some stuff")
+			time.Sleep(1 * time.Second)
+		}()
+		
+		go func() {
+			// Call lock, which will block if there aren't free locks
+			// and defer the unlock until the function ends
+			S.Lock()
+			defer S.Unlock()
+		
+			// Do some other stuff
+			fmt.Println("Doing some other stuff")
+			time.Sleep(50 * time.Millisecond)
+		}()
+		
+		time.Sleep(1 * time.Millisecond)
+		fmt.Printf("Free locks? %d\n",S.Free())
+		time.Sleep(3 * time.Second)
+		fmt.Printf("Free locks now? %d\n",S.Free())
+	}
 */
 package semaphore
 
